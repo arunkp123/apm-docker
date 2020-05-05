e53432c66b6d29f3d02567f631fb8cb414c50b29
arunkp123 <arunkp123@live.in>
2020-05-05 20:40:42 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo " $red ----- Setup by arunkp123 ------- $white "
+echo " $red ----- Installing Pre requisites ------- $white "
+echo "$red ---- Clone repo"
+sudo docker-compose down && docker-compose up --build -dclear
