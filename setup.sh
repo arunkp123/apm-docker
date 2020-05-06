36cb950397dbea2db3590c9a02c3312bb853cb6a
Arun Prasad <arun.prasad@nagarro.com>
2020-05-06 18:00:51 +0530

    Setup upograded to install apache,php,mysql,phpmyadmin

--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
-echo " $red ----- Setup by arunkp123 ------- $white "
-echo " $red ----- Installing Pre requisites ------- $white "
-echo "$red ---- Clone repo"
-sudo docker-compose down && docker-compose up --build -dclear
+source ./.env
+echo " $red ########## Setup by arunkp123 ###############"
+echo "Installing Pre requisites... "
+echo "Enter system password if asked!"
+echo "Making directory $PROJECT_PATH "
+mkdir $PROJECT_PATH
+mkdir apache
+mkdir php
+sudo -- sh -c "echo '127.0.0.1 $DOMAIN' >> /etc/hosts"
+
+
+echo ""
+read -p "Do you want to reset docker and install(y/n)? " CONT
+if [ "$CONT" = "y" ]; then
+ docker system prune -a;
+ 
+else
+  echo "Docker reset skipped...";
+fi
+
+docker-compose down && docker-compose up --build -d
+
+
