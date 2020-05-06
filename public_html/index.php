c04982d54c5ed5aa381f685a03309d27e6ae62fb
Arun Prasad <arun.prasad@nagarro.com>
2020-05-07 00:24:10 +0530

    Update setup

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,4 @@
-<h1>Hello Cloudreach!</h1>
+<h1>Hello from APMP-Docker!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
 $host = 'mysql';
@@ -6,10 +6,13 @@ $user = 'root';
 $pass = 'root';
 $conn = new mysqli($host, $user, $pass);
 
+echo "Testing mysql connection...<br><br>";
 if ($conn->connect_error) {
+    echo "Failed connecting to mysql:<br>";
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected to MySQL successfully!";
+    echo "Connected to MySQL successfully!<br>";
+    echo "Mysql Test passed!";
 }
  
 ?>
